c5a388fb43e534ec7acd17e2db01f6e6467df54e
Ekaterina Kozhina <katerinabonia@mail.ru>
2024-11-07 15:32:46 +0300

    Changed menu

--- script/menu.js
+++ script/menu.js
@@ -1,6 +1,7 @@
 (function() {
     const navLinks = document.querySelectorAll('nav ul li a');
     const currentLocation = document.location.href;
+
     navLinks.forEach(link => {
         if (link.href === currentLocation) {
             link.classList.add('active');
