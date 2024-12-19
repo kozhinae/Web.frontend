a1f46a8bdd44ebaa48d3910a9500a088b4ac88c0
Ekaterina Kozhina <katerinabonia@mail.ru>
2024-12-19 12:58:41 +0300

    lab7

---
+++ script/modalwindow.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 5,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
+
+document.querySelectorAll('.swiper-slide img').forEach((img) => {
+    img.addEventListener('click', () => {
+        Swal.fire({
+            title: img.alt,
+            text: 'Информация об изображении',
+            imageUrl: img.src,
+            imageWidth: 50,
+            imageHeight: 10,
+            imageAlt: img.alt,
+            confirmButtonText: 'Закрыть',
+        });
+    });
+});
